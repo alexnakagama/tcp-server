31b545b15b1a369ed0f8a6fdbe39b2c09b71f8ef
alexnakagama <alexnakagama4@gmail.com>
2026-07-03 23:57:17 -0300

    created clients header file

--- include/client/client.h
+++ include/client/client.h
@@ -1,4 +1,14 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
 typedef struct Client {
-    int sockfd;
-    char name[32];
+    int sockfd; // socket (connection id)
+    struct sockaddr_in address; // ip + port of that client
+    char name[32]; // username
 } Client;
+
+Client *create_client(void);
+void delete_client(Client *client);
+void accept_client(Client *client);
+void broadcast_message(char *msg);
