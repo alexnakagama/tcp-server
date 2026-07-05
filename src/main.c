1038820935d17e03e69758528a40d5ca3e2c70da
alexnakagama <alexnakagama4@gmail.com>
2026-07-05 18:34:22 -0300

    starting the server

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
 #include <sys/socket.h>
 
+#include "server/server.h"
+
+#define SERVER_PORT 8080
+
 int main(void) {
+    Server server;
+
+    // Starts the server on the stack
+    if (server_init(&server, SERVER_PORT) != 0) {
+        printf("Server couldnt be started\n");
+        return -1;
+    }
+
     return 0;
 }
