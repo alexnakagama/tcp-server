8bde4dc853fa1106a1fc7cf732a58d7947f27609
alexnakagama <alexnakagama4@gmail.com>
2026-07-07 20:42:02 -0300

    deleted it from header file

--- include/server/server.h
+++ include/server/server.h
@@ -22,7 +22,6 @@ typedef struct Server {
 
 int server_init(Server *server, uint16_t server_port);
 int server_accept(Server *server, Client *client);
-void server_run(Server *server);
 void server_broadcast(Server *server, Client *sender, const char *msg);
 void server_close(Server *server);
 
