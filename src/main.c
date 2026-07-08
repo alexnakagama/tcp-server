a89e230d4d4e1ae77a01fda7940c52915cf83774
alexnakagama <alexnakagama4@gmail.com>
2026-07-08 15:52:46 -0300

    implementation on main

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "client/client.h"
 #include "server/server.h"
 
+#define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
 
 int main(void) {
@@ -17,15 +18,17 @@ int main(void) {
     }
 
     while (1) {
+        // 1. Wait for the client
         Client *client = server_accept(&server);
-
         if (!client) continue;
 
+        // 2. Save it
         if (server_add_client(&server, client) != 0) {
             delete_client(client);
             continue;
         } 
 
+        // 3. Receive username
         if (client_receive_username(client) != 0) {
             server_remove_client(&server, client);
             delete_client(client);
@@ -34,17 +37,26 @@ int main(void) {
 
         printf("%s joined the server\n", client->name);
 
-        // Receive messages
-        // Broadcast messages
-        //Client disconnects
+        // 4. Chat
+        char buffer[1024];
 
+        while (1) {
+            int bytes = recv(client->sockfd, buffer, sizeof(buffer) - 1, 0);
+            if (bytes <= 0) break;
+
+            buffer[bytes] = '\0';
+
+            printf("%s: %s\n", client->name, buffer);
+
+            server_broadcast(&server, client, buffer);
+        }
+
+        // 5. Client disconnected
         server_remove_client(&server, client);
-
         delete_client(client);
     }
 
     server_close(&server);
-
     return EXIT_SUCCESS;
 }
 
