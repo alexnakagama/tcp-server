bc8d1de27f835e3e03d47d1ad75e04be926da317
alexnakagama <alexnakagama4@gmail.com>
2026-07-08 14:35:59 -0300

    implementation of the func in main

--- src/main.c
+++ src/main.c
@@ -26,23 +26,14 @@ int main(void) {
             continue;
         } 
 
-        char username[32];
-
-        int bytes = recv(client->sockfd, username, sizeof(username) - 1, 0);
-
-        if (bytes <= 0) {
+        if (client_receive_username(client) != 0) {
             server_remove_client(&server, client);
             delete_client(client);
             continue;
-        } 
-
-        username[bytes] = '\0';
-
-        assign_username(client, username);
+        }
 
         printf("%s joined the server\n", client->name);
 
-        // Receive username
         // Receive messages
         // Broadcast messages
         //Client disconnects
