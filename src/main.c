c6611848dfe724d6f988b6c765169b7e952f608f
alexnakagama <alexnakagama4@gmail.com>
2026-07-08 14:22:15 -0300

    implementation on main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 
+#include "client/client.h"
 #include "server/server.h"
 
 #define SERVER_PORT 8080
@@ -11,11 +13,31 @@ int main(void) {
     // Starts the server on the stack
     if (server_init(&server, SERVER_PORT) != 0) {
         printf("Server couldnt be started\n");
-        return -1;
+        return EXIT_FAILURE;
     }
 
     while (1) {
+        Client *client = server_accept(&server);
+
+        if (!client) continue;
+
+        if (server_add_client(&server, client) != 0) {
+            delete_client(client);
+            continue;
+        } 
+
+        // Receive username
+        // Receive messages
+        // Broadcast messages
+        //Client disconnects
+
+        server_remove_client(&server, client);
+
+        delete_client(client);
     }
 
-    return 0;
+    server_close(&server);
+
+    return EXIT_SUCCESS;
 }
+
